ec36eb05dd3aef8babb19cef44503a763979454e
lzw981731 <827520890@qq.com>
2020-06-26 22:04:09 +0800

    Add files via upload

---
+++ version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export FRPS_VER=0.33.0
+export FRPS_INIT="https://dl-web.dropbox.com/s/pthyz8ha817943o/frps.init"
+export github_download_url="https://github.com/lzw981731/frp/releases/download"
